863be847f774a73cbad086252d1ca01226fb0106
DeNiro Dumas <denirod@Deniros-MacBook-Pro.local>
2022-06-18 09:03:48 -0400

    final product

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,7 +3,7 @@ const { Comment, Pizza } = require('../models');
 const commentController = {
   // add comment to pizza
   addComment({ params, body }, res) {
-    console.log(body);
+    console.log(params);
     Comment.create(body)
       .then(({ _id }) => {
         return Pizza.findOneAndUpdate(
@@ -13,6 +13,7 @@ const commentController = {
         );
       })
       .then(dbPizzaData => {
+        console.log(dbPizzaData);
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
           return;
@@ -22,32 +23,22 @@ const commentController = {
       .catch(err => res.json(err));
   },
 
-  addReply({params, body},res){
+  // add reply to comment
+  addReply({ params, body }, res) {
     Comment.findOneAndUpdate(
-      {_id:params.commentId},
-      {$push : {replies:body}},
-      {new:true}
+      { _id: params.commentId },
+      { $push: { replies: body } },
+      { new: true, runValidators: true }
     )
-    .then(dbPizzaData => {
-      if(!dbPizzaData){
-        res.status(404).json({message: 'No pizza found with this id!'});
-        return;
-      }
-      res.json(dbPizzaData);
-    })
-    .catch(err => res.json(err));
+      .then(dbPizzaData => {
+        if (!dbPizzaData) {
+          res.status(404).json({ message: 'No pizza found with this id!' });
+          return;
+        }
+        res.json(dbPizzaData);
+      })
+      .catch(err => res.json(err));
   },
-
-  removeReply({params},res){
-    Comment.findOneAndUpdate(
-      {_id:params.commentId},
-      {$pull : {replies:{replyId: params.replyId}}},
-      {new:true}
-    )
-    .then(dbPizzaData => res.json(dbPizzaData))
-    .catch(err => res.json(err));
-  },
-
 
   // remove comment
   removeComment({ params }, res) {
@@ -70,7 +61,18 @@ const commentController = {
         res.json(dbPizzaData);
       })
       .catch(err => res.json(err));
+  },
+  // remove reply
+  removeReply({ params }, res) {
+    Comment.findOneAndUpdate(
+      { _id: params.commentId },
+      { $pull: { replies: { replyId: params.replyId } } },
+      { new: true }
+    )
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.json(err));
   }
 };
 
 module.exports = commentController;
+
--- models/Comment.js
+++ models/Comment.js
@@ -3,58 +3,63 @@ const dateFormat = require('../utils/dateFormat');
 
 const ReplySchema = new Schema(
   {
-    replyId:{
-      // set custom id to avoid confusion with parent comment _id
+    // set custom id to avoid confusion with parent comment _id
+    replyId: {
       type: Schema.Types.ObjectId,
       default: () => new Types.ObjectId()
     },
-
-    replyBody:{
-      type:String
+    replyBody: {
+      type: String,
+      required: true
     },
-    writtenBy:{
-      type:String
+    writtenBy: {
+      type: String,
+      required: true,
+      trim: true
     },
-    createdAt:{
-      type:Date,
-      default:Date.now,
+    createdAt: {
+      type: Date,
+      default: Date.now,
       get: createdAtVal => dateFormat(createdAtVal)
     }
   },
   {
-    toJSON:{
-      getters:true
+    toJSON: {
+      getters: true
     }
   }
 );
 
-
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+      required: true
+    },
+    commentBody: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    },
+    // use ReplySchema to validate data for a reply
+    replies: [ReplySchema]
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: createdAtVal => dateFormat(createdAtVal)
-  },
-  replies:[ReplySchema]
-},
-{
-  toJSON:{
-    virtuals:true,
-    getters:true
-  },
-  id:false
-}
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
 );
 
-CommentSchema.virtual('replyCount').get(function(){
+CommentSchema.virtual('replyCount').get(function() {
   return this.replies.length;
-})
+});
 
 const Comment = model('Comment', CommentSchema);
 
